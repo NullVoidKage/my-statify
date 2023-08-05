2a6a49db566ca49158d3976820a0bfdf9faed47c
Nikko <nickouchiha5@gmail.com>
2023-08-05 20:41:45 +0800

    Added console log

--- api/delete-user.ts
+++ api/delete-user.ts
@@ -5,16 +5,19 @@ export default async function handler(
   request: VercelRequest,
   response: VercelResponse,
 ) {
-  const { SpotifyUserID } = request.body; // Change this line to receive the Spotify ID
+  const { SpotifyUserID } = request.query; // Change this line to receive the Spotify ID
 
   if (!SpotifyUserID) {
     return response.status(400).json({ error: 'SpotifyID is required' }); // Change the error message accordingly
   }
 
   try {
+    // Convert SpotifyUserID to string if it's an array
+    const spotifyUserIdString = Array.isArray(SpotifyUserID) ? SpotifyUserID[0] : SpotifyUserID;
+
     const result = await sql`
       DELETE FROM STATIFY_DB
-      WHERE SpotifyID = ${SpotifyUserID}; // Change this line to match the Spotify ID
+      WHERE SpotifyID = ${spotifyUserIdString}; // Change this line to match the Spotify ID
     `;
 
     // Check the number of affected rows to determine if the deletion was successful
