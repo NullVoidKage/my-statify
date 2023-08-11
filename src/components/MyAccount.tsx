3b66234049209ec14fb7ce6ac58c99d4f0d143d2
Nikko <nickouchiha5@gmail.com>
2023-08-11 11:15:44 +0800

    Added new condition for delete

--- src/components/MyAccount.tsx
+++ src/components/MyAccount.tsx
@@ -1,9 +1,9 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import "../style/MyAccount.scss";
-import { FaTrash } from 'react-icons/fa';
+import { FaTrash } from "react-icons/fa";
 import { useNavigate } from "react-router-dom";
-import Spinner from './Spinner';
+import Spinner from "./Spinner";
 
 interface MyAccountProps {
   token: string | null;
@@ -13,25 +13,34 @@ interface MyAccountProps {
   onLogout: () => void;
 }
 
-export function MyAccount({ token, userPhoto , userId, userName, onLogout}: MyAccountProps) {
+export function MyAccount({
+  token,
+  userPhoto,
+  userId,
+  userName,
+  onLogout,
+}: MyAccountProps) {
   const [data, setData] = useState<any>(null);
   const [statifyData, setStatifyData] = useState<any>(null);
   const [error, setError] = useState<string | null>(null);
   const [isLoading, setIsLoading] = useState<boolean>(true);
   const [showDeleteModal, setShowDeleteModal] = useState<boolean>(false);
-  const [isLoggingOut, setIsLoggingOut] = useState(false); 
+  const [isLoggingOut, setIsLoggingOut] = useState(false);
   const navigate = useNavigate();
-
+  const [confirmInput, setConfirmInput] = useState("");
 
   useEffect(() => {
     const fetchUserData = async () => {
       try {
-        const { data: spotifyData } = await axios.get("https://api.spotify.com/v1/me", {
-          headers: {
-            Authorization: `Bearer ${token}`,
-            "Accept-Encoding": "gzip",
-          },
-        });
+        const { data: spotifyData } = await axios.get(
+          "https://api.spotify.com/v1/me",
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+              "Accept-Encoding": "gzip",
+            },
+          }
+        );
 
         setData(spotifyData);
       } catch (err: any) {
@@ -43,22 +52,23 @@ export function MyAccount({ token, userPhoto , userId, userName, onLogout}: MyAc
 
     const fetchStatifyData = async () => {
       try {
-        const response = await axios.get(`https://my-statify.vercel.app/api/select-user?SpotifyUserID=${userId}`);
+        const response = await axios.get(
+          `https://my-statify.vercel.app/api/select-user?SpotifyUserID=${userId}`
+        );
 
-        console.log(response.data)
+        console.log(response.data);
         setStatifyData(response.data);
-      }
-      catch(error: any){
+      } catch (error: any) {
         setError(error.message);
       }
-    }
+    };
     fetchStatifyData();
 
     fetchUserData();
   }, [token]);
 
   const handleLogout = () => {
-    console.log('Called Logout')
+    console.log("Called Logout");
     setIsLoggingOut(true); // Set the logging out state to true
     setTimeout(() => {
       setIsLoggingOut(false); // Reset the logging out state after 2 seconds
@@ -68,81 +78,103 @@ export function MyAccount({ token, userPhoto , userId, userName, onLogout}: MyAc
     }, 2000);
   };
 
-
-
   const deleteAccount = async () => {
     try {
       // Send a DELETE request to the Vercel API endpoint for deleting the user
-      const response = await axios.delete(`https://my-statify.vercel.app/api/delete-user?SpotifyUserID=${userId}`);
+      const response = await axios.delete(
+        `https://my-statify.vercel.app/api/delete-user?SpotifyUserID=${userId}`
+      );
 
-      console.log(userId)
+      console.log(userId);
       console.log(response);
-  
+
       if (response.status === 200) {
         handleLogout();
-        console.log('Account deleted successfully');
+        console.log("Account deleted successfully");
       } else {
-        console.error('An error occurred while deleting the account.');
+        console.error("An error occurred while deleting the account.");
       }
     } catch (error) {
-      console.error('An error occurred while deleting the account.');
+      console.error("An error occurred while deleting the account.");
       console.error(error);
     }
   };
-  
+
   const formatDate = (dateString: any) => {
     const date = new Date(dateString);
     const year = date.getFullYear();
-    const month = date.toLocaleString('default', { month: 'long' });
+    const month = date.toLocaleString("default", { month: "long" });
     const day = date.getDate();
-    const time = date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+    const time = date.toLocaleTimeString([], {
+      hour: "2-digit",
+      minute: "2-digit",
+    });
     return `${month} ${day}, ${year} ${time}`;
   };
 
-  
-
   return (
     <div className="my-account-app">
-  
-      <div className='account-container'>
-      {isLoading ? (
-        <div>Loading...</div>
-      ) : error ? (
-        <div>Error: {error}</div>
-      ) : (
-        // Render your data here
-        <div className='account-content'>
-          <img src={userPhoto} alt="User Photo" />
-          <div className='welcome-user'>Welcome, {data?.display_name}</div>
-          <div className='account1'>Spotify ID: {data?.id}</div>
-          <div className='account1'>Statify ID: {statifyData?.id}</div>
-          <div className='account1'>Created At: {formatDate(statifyData?.createdat)}</div>
-          <div className='delete-button' >Delete statify account <FaTrash onClick={() => setShowDeleteModal(true)}/></div>
-        </div>
-      )}
-      <div>
-    
-      </div>
-     
+      <div className="account-container">
+        {isLoading ? (
+          <div>Loading...</div>
+        ) : error ? (
+          <div>Error: {error}</div>
+        ) : (
+          // Render your data here
+          <div className="account-content">
+            <img src={userPhoto} alt="User Photo" />
+            <div className="welcome-user">Welcome, {data?.display_name}</div>
+            <div className="account1">Spotify ID: {data?.id}</div>
+            <div className="account1">Statify ID: {statifyData?.id}</div>
+            <div className="account1">
+              Created At: {formatDate(statifyData?.createdat)}
+            </div>
+            <div className="delete-button">
+              Delete statify account{" "}
+              <FaTrash onClick={() => setShowDeleteModal(true)} />
+            </div>
+          </div>
+        )}
+        <div></div>
       </div>
 
-        
       {showDeleteModal && (
-       <div className="modal-overlay">
-       <div className="delete-modal">
-         <div className="delete-my-account">Delete My Statify Account?</div>
-         <div className='are-you-sure'>Are you sure you want to delete your account completely?</div>
-      
-         <div className="modal-buttons">
-           <button className="btn-cancel" onClick={() => setShowDeleteModal(false)}>Cancel</button>
-           <button  onClick={deleteAccount}>
-             Delete Account
-           </button>
-         </div>
-       </div>
-     </div>
+        <div className="modal-overlay">
+          <div className="delete-modal">
+            <div className="delete-my-account">Delete My Statify Account?</div>
+            <div className="are-you-sure">
+              Are you sure you want to delete your account completely?
+            </div>
+            <input
+              type="text"
+              placeholder="Type 'DELETE' to confirm"
+              value={confirmInput}
+              onChange={(e) => setConfirmInput(e.target.value)}
+            />
+            {confirmInput !== "DELETE" && (
+              <div className="error-message">
+                Please type 'DELETE' to confirm
+              </div>
+            )}
+
+            <div className="modal-buttons">
+              <button
+                className="btn-cancel"
+                onClick={() => setShowDeleteModal(false)}
+              >
+                Cancel
+              </button>
+              <button
+                onClick={deleteAccount}
+                disabled={confirmInput !== "DELETE"}
+              >
+                Delete Account
+              </button>
+            </div>
+          </div>
+        </div>
       )}
-         {isLoggingOut && <Spinner />}
+      {isLoggingOut && <Spinner />}
     </div>
   );
 }
